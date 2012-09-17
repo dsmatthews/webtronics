4a4a64c083cb9e3cf059d824a9ddfc898220344a
wikitronic <wikitronic@0cb9cdde-926e-5f4b-b0f7-b7944dfa628e>
2012-09-17 19:39:33 +0000

    attempted ie9 save svg fix

--- webtronics/script/gui.js
+++ webtronics/script/gui.js
@@ -440,7 +440,8 @@ var webtronics={
 //            $("webtronics_image_save").src="data:image/svg+xml;base64;charset=utf-8," + encode64(webtronics.getMarkup());
             var svg = webtronics.getMarkup();
             console.log(svg);
-           $("webtronics_image_save").src="data:image/svg+xml;base64," + encode64(svg);
+            $("webtronics_image_save").src="data:image/svg+xml;base64," + encode64(svg);
+            $("webtronics_image_save").type="image/svg+xml";
             $('webtronics_file_menu').style.display='none';
         },
 
