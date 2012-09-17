cfd571ae654a4aeac7c160a018f1321f5e844ac2
wikitronic <wikitronic@0cb9cdde-926e-5f4b-b0f7-b7944dfa628e>
2012-09-17 13:19:08 +0000

    change mime-types

--- webtronics/script/utils.js
+++ webtronics/script/utils.js
@@ -6,39 +6,43 @@
 //           also precalculate the size of the array needed.
 
 
-	 function encode64(input) {
-        if(window.btoa)return window.btoa(input);
-		// base64 strings are 4/3 larger than the original string
-        var output = new Array( Math.floor( (input.length + 2) / 3 ) * 4 );
-		var chr1, chr2, chr3;
-		var enc1, enc2, enc3, enc4;
-		var i = 0, p = 0;
-    	var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function encode64(input) {
+    if(window.btoa){
+        return window.btoa(input);
+    }
+// base64 strings are 4/3 larger than the original string
+    var output = new Array( Math.floor( (input.length + 2) / 3 ) * 4 );
+    var chr1, chr2, chr3;
+    var enc1, enc2, enc3, enc4;
+    var i = 0, p = 0;
+    var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
-		do {
-			chr1 = input.charCodeAt(i++);
-			chr2 = input.charCodeAt(i++);
-			chr3 = input.charCodeAt(i++);
+    do {
+        chr1 = input.charCodeAt(i++);
+        chr2 = input.charCodeAt(i++);
+        chr3 = input.charCodeAt(i++);
+        
+        enc1 = chr1 >> 2;
+        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+        enc4 = chr3 & 63;
+        
+        if (isNaN(chr2)) {
+        	enc3 = enc4 = 64;
+        }
+        else if (isNaN(chr3)) {
+    	    enc4 = 64;
+        }
 
-			enc1 = chr1 >> 2;
-			enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-			enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-			enc4 = chr3 & 63;
+        output[p++] = _keyStr.charAt(enc1);
+        output[p++] = _keyStr.charAt(enc2);
+        output[p++] = _keyStr.charAt(enc3);
+        output[p++] = _keyStr.charAt(enc4);
+    } while (i < input.length);
 
-			if (isNaN(chr2)) {
-				enc3 = enc4 = 64;
-			} else if (isNaN(chr3)) {
-				enc4 = 64;
-			}
+    return output.join('');
+}
 
-			output[p++] = _keyStr.charAt(enc1);
-			output[p++] = _keyStr.charAt(enc2);
-			output[p++] = _keyStr.charAt(enc3);
-			output[p++] = _keyStr.charAt(enc4);
-		} while (i < input.length);
-
-		return output.join('');
-	}
 function createUUID()
 {
   return [7].map(function(length) {
