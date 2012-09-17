f0dca5c2dcbcd91390ef783d1c7cfdfbe6922bae
wikitronic <wikitronic@0cb9cdde-926e-5f4b-b0f7-b7944dfa628e>
2012-09-17 19:08:05 +0000

    attempted ie9 save svg fix

--- webtronics/script/schematic.js
+++ webtronics/script/schematic.js
@@ -611,6 +611,7 @@ Schematic.prototype.getMarkup = function() {
     var doc=xmlDoc = document.implementation.createDocument("", "", null);
 	var svg = doc.createElementNS(this.svgNs, "svg");
 	svg.setAttribute('xmlns',this.svgNs);
+    doc.appendChild(svg);
 //	svg.setAttribute('xmlns:wtx',this.wtxNs);
     var bg=doc.createElementNS(this.svgNs,'rect');
     bg.setAttribute('x',0);
