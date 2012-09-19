da7b9680e761665fe08574e3d9f0aca42688e324
wikitronic <wikitronic@0cb9cdde-926e-5f4b-b0f7-b7944dfa628e>
2012-09-19 00:20:23 +0000

    fix chrome png error

--- webtronics/script/gui.js
+++ webtronics/script/gui.js
@@ -604,13 +604,7 @@ I want to preserve the css color for inverted diagrams in png
             var drawing=(new XMLSerializer()).serializeToString(doc);
             console.log(drawing);
             
-            try{
-                ctx.drawSvg(doc, 0, 0, svgsize.width+10,'height',svgsize.height+10);    
-            }       	    
-            catch(e){
-                ctx.drawSvg(drawing, 0, 0, svgsize.width+10,'height',svgsize.height+10);    
-            
-            }
+            ctx.drawSvg(doc, 0, 0, svgsize.width+10,svgsize.height+10);    
             this.center($('webtronics_image'));
             var url= canvas.toDataURL("image/png");
             $("webtronics_image_save").src=url;
